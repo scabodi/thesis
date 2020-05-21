ca254e40c880aa5d000cdd019ed0194f01a78fb3
scabodi <sara.cabodi@hotmail.it>
2020-05-21 19:47:48 +0200

    data update

--- compute_network_measures.py
+++ compute_network_measures.py
@@ -1,4 +1,5 @@
 import network_functions as nf
+import networkx as nx
 
 if __name__ == '__main__':
 
@@ -20,6 +21,11 @@ if __name__ == '__main__':
         ''' CREATE NETWORK - undirected and unweighted and add it to the proper dictionary '''
         net = nf.create_network(city, types=types)
 
+        c0 = nx.average_clustering(net, count_zeros=True)
+        c1 = nx.average_clustering(net, count_zeros=False)
+        print(c0)
+        print(c1)
+
         # r = nf.get_assortativity(net)
         # additional_measures['<r>'].append(r)
         #
@@ -32,29 +38,29 @@ if __name__ == '__main__':
         # ''' plot the network without difference between types of transport '''
         # nf.plot_network(city, net)
 
-        ''' Compute centrality measures '''
-        [degree, betweenness, closeness, eigenvector] = nf.get_centrality_measures(net, tol)
-        centrality_dict[city] = {
-            'degree': list(degree),
-            'betweenness': list(betweenness),
-            'closeness': list(closeness),
-            'eigenvector': list(eigenvector),
-        }
-
-        file_name = './results/'+city+'/centrality_measures.json'
-        nf.dump_json(file_name, centrality_dict[city])
-
-        ''' Plot measures against area of specific city '''
-        # for each city compute certain measures - first time saved them into a file and then retrieve them each time
-        measures_dict[city] = {}
-        nf.compute_measures(net, measures_dict[city])
-        print(measures_dict[city])
-
-        ''' Compute degree distribution for each city '''
-        # degree -- list of nodes degree
-        degrees[city] = [v for k, v in net.degree().items()]
-
-    if dump:
-        nf.dump_json(prefix_json+'degrees.json', degrees)
-        nf.dump_json(prefix_json+'centrality_measures.json', centrality_dict)
-        nf.dump_json(prefix_json+'additional_measures.json', additional_measures)
+    #     ''' Compute centrality measures '''
+    #     [degree, betweenness, closeness, eigenvector] = nf.get_centrality_measures(net, tol)
+    #     centrality_dict[city] = {
+    #         'degree': list(degree),
+    #         'betweenness': list(betweenness),
+    #         'closeness': list(closeness),
+    #         'eigenvector': list(eigenvector),
+    #     }
+    #
+    #     file_name = './results/'+city+'/centrality_measures.json'
+    #     nf.dump_json(file_name, centrality_dict[city])
+    #
+    #     ''' Plot measures against area of specific city '''
+    #     # for each city compute certain measures - first time saved them into a file and then retrieve them each time
+    #     measures_dict[city] = {}
+    #     nf.compute_measures(net, measures_dict[city])
+    #     print(measures_dict[city])
+    #
+    #     ''' Compute degree distribution for each city '''
+    #     # degree -- list of nodes degree
+    #     degrees[city] = [v for k, v in net.degree().items()]
+    #
+    # if dump:
+    #     nf.dump_json(prefix_json+'degrees.json', degrees)
+    #     nf.dump_json(prefix_json+'centrality_measures.json', centrality_dict)
+    #     nf.dump_json(prefix_json+'additional_measures.json', additional_measures)
